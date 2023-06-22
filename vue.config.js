5cb8be10da675f0f8b8e120dd0cb146e17a8b09a
Muhammad Rafli Jatnika <rafli@bts.id>
2023-06-23 01:12:11 +0700

    improvement(config): improvement package and vue config

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   productionSourceMap: false,
-  chainWebpack: () => {},
+  chainWebpack: (config) => {
+    config.plugin('html').tap((args) => {
+      args[0].title = `Roketin Test Case Frontend`;
+      return args;
+    });
+  },
   transpileDependencies: ['vuetify'],
   css: {
     loaderOptions: {
